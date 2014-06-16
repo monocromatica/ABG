a0cd63267df9cda68c26253e3b23011ac6d49cfc
monocromatica <hola@monocromatica.mx>
2014-06-16 14:05:02 -0500

    Ootimizada

---
+++ Build/formularioABG/validation_data.js
@@ -0,0 +1,3 @@
+ var data_validation = '{"fb-submit-button":{"hover":{"background-image":""}},"item2_text_1":{"required":true},"item3_text_1":{"required":true},"item4_text_1":{},"item5_text_1":{},"item6_select_1":{},"item7_text_1":{},"item8_textarea_1":{"maxlength":"10000","required":true}}';
+ var data_jsplugins = '[]';
+ var data_cssplugins = '[]';
